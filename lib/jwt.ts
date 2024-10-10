6b8835dcab09898d14675145cfbae7f5ea14ce69
Julien Knebel <Julien@DESKTOP-BUCLD9G>
2024-10-10 04:40:04 +0200

    fix TS issues

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,6 +1,10 @@
-import { SignJWT, jwtVerify } from "jose";
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
 
-export async function signToken(userId: string): any {
+export async function signToken(userId: string): Promise<string> {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
   return new SignJWT({ userId })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
@@ -8,15 +12,19 @@ export async function signToken(userId: string): any {
     .sign(new TextEncoder().encode(process.env.JWT_SECRET));
 }
 
-export async function verifyToken(token: string): any {
+export async function verifyToken(token: string): Promise<JWTPayload | null> {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+
   try {
-    const payload = await jwtVerify(
+    const { payload } = await jwtVerify(
       token,
       new TextEncoder().encode(process.env.JWT_SECRET)
     );
     return payload;
   } catch (error) {
-    console.log(error);
+    console.error("Token verification failed:", error);
     return null;
   }
 }
